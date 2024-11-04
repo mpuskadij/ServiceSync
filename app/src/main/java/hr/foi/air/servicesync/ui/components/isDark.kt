5c9e9a11cbeb93363d63a59da4d2449bd123c8ad
Duje Blažević <dblazevic21@student.foi.hr>
2024-11-04 22:49:28 +0100

    Implemented navigation bar with dynamic coloring. Added isDark.kt for redundancy fixes. Changed some minor visual issues and added a potential implementation for BadgeBoxes on the downer icons.

--- app/src/main/java/hr/foi/air/servicesync/ui/components/isDark.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/components/isDark.kt
@@ -3,8 +3,6 @@ package hr.foi.air.servicesync.ui.components
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.example.compose.primaryDarkHighContrast
-import com.example.compose.primaryLightHighContrast
 
 @Composable
 fun isDark(primary: Color, secondary: Color) = if (isSystemInDarkTheme()) primary else secondary
--- app/src/main/java/hr/foi/air/servicesync/ui/screens/SearchScreen.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/screens/SearchScreen.kt
@@ -1,24 +1,19 @@
 package hr.foi.air.servicesync.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.compose.onPrimaryDark
 import com.example.compose.onPrimaryLight
 import com.example.compose.primaryDark
-import com.example.compose.primaryDarkHighContrast
 import com.example.compose.primaryLight
-import com.example.compose.primaryLightHighContrast
 import hr.foi.air.servicesync.ui.components.isDark
 
 @Composable

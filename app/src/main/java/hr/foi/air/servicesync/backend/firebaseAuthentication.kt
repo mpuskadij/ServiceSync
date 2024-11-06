fb520a82b03e72a1e7095662f6b6f64d97ac5154
dpetek21 <dpetek21@foi.hr>
2024-11-06 13:05:10 +0100

    FTR-32: -Added a function that checks the login information and then puts it into the public variable currentUser within the firebaseAuthentication class for the purpose of further usage

--- app/src/main/java/hr/foi/air/servicesync/backend/firebaseAuthentication.kt
+++ app/src/main/java/hr/foi/air/servicesync/backend/firebaseAuthentication.kt
@@ -6,7 +6,7 @@ import android.util.Log
 
 class firebaseAuthentication {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
+    public var currentUser : FirebaseUser? = null
     fun registerUser(email: String, password: String, callback: (Boolean, FirebaseUser?) -> Unit) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -16,6 +16,19 @@ class firebaseAuthentication {
                     callback(true, user)
                 } else {
                     Log.e("FirebaseAuth", "User registration failed: ${task.exception?.message}")
+                    callback(false, null)
+                }
+            }
+    }
+    fun loginUser(email: String, password: String, callback: (Boolean, FirebaseUser?) -> Unit) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    currentUser = auth.currentUser
+                    Log.d("FirebaseAuth", "User successfuly logged in: ${currentUser?.email}")
+                    callback(true, currentUser)
+                } else {
+                    Log.e("FirebaseAuth", "Login failed: ${task.exception?.message}")
                     callback(false, null)
                 }
             }

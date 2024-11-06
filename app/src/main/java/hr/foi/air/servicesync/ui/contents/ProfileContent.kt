209c659c37c4eea6f23c56e58d4623fa3e6d6eac
Ivan Beusan <ivan.beusan@yahoo.com>
2024-11-06 22:30:43 +0100

    FTR-27: Removed unnecessary import directive.

--- app/src/main/java/hr/foi/air/servicesync/ui/contents/ProfileContent.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/contents/ProfileContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue

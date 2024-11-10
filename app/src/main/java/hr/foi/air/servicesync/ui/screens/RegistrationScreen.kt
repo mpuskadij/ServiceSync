5288cde1dd55d84b4020bfaf0f8bad576ba00753
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-11-10 17:49:54 +0100

    FTR-51 Changed logo on login and registration screen

--- app/src/main/java/hr/foi/air/servicesync/ui/screens/RegistrationScreen.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/screens/RegistrationScreen.kt
@@ -1,6 +1,7 @@
 package hr.foi.air.servicesync.ui.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -24,10 +26,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hr.foi.air.servicesync.R
 import hr.foi.air.servicesync.business.loginRegisterHandler
 
 @Composable
@@ -58,16 +62,15 @@ fun RegistrationScreen(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ){
-                    Text(
-                        text = "ServiceSync",
-                        style = MaterialTheme.typography.displayLarge,
+                    Image(
+                        painter = painterResource(R.drawable.logo),
+                        contentDescription = "ServiceSync logo",
+                        modifier = Modifier
+                            .size(280.dp)
+                            .padding(8.dp)
                     )
-                    Text(
-                        text = "Sve na jednom mjestu",
-
-                        )
                 }
-                Spacer(modifier = Modifier.height(48.dp))
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(
                     modifier = Modifier.padding(16.dp),
                     text = "Registracija",

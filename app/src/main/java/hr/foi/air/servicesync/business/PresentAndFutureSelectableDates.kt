6324970ded7842435ea5272e784fd98d49fe3925
Marin Puškadija <marin.puskadija@gmail.com>
2024-11-28 18:10:18 +0100

    FTR-66: -fixed class ServiceReservationScreen because it was not allowing for current date to be selected

--- app/src/main/java/hr/foi/air/servicesync/business/PresentAndFutureSelectableDates.kt
+++ app/src/main/java/hr/foi/air/servicesync/business/PresentAndFutureSelectableDates.kt
@@ -3,11 +3,14 @@ package hr.foi.air.servicesync.business
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SelectableDates
 import java.time.LocalDate
+import java.time.ZoneOffset
 
 @OptIn(ExperimentalMaterial3Api::class)
 class PresentAndFutureSelectableDates : SelectableDates {
+    private val now = LocalDate.now()
+    private val dayStart = now.atTime(0, 0, 0, 0).toEpochSecond(ZoneOffset.UTC) * 1000
     override fun isSelectableDate(utcTimeMillis: Long): Boolean {
-        return utcTimeMillis >= System.currentTimeMillis()
+        return utcTimeMillis >= dayStart
     }
 
     override fun isSelectableYear(year: Int): Boolean {

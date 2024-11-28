b99161c77cb0c74b04fbca9eca8363dbe780a22b
Marin Puškadija <marin.puskadija@gmail.com>
2024-11-28 18:04:23 +0100

    FTR-66: -added class PresentAndFutureSelectableDates which implements SelectableDates interface so that DatePicker disables past dates

---
+++ app/src/main/java/hr/foi/air/servicesync/business/PresentAndFutureSelectableDates.kt
@@ -0,0 +1,16 @@
+package hr.foi.air.servicesync.business
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
+import java.time.LocalDate
+
+@OptIn(ExperimentalMaterial3Api::class)
+class PresentAndFutureSelectableDates : SelectableDates {
+    override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+        return utcTimeMillis >= System.currentTimeMillis()
+    }
+
+    override fun isSelectableYear(year: Int): Boolean {
+        return year >= LocalDate.now().year
+    }
+}

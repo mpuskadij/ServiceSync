4d3c80d99b2a2e0509af983e37d4769050c1eaca
dpetek21 <dpetek21@foi.hr>
2024-12-04 13:31:42 +0100

    QuickFix : -OptIn needed for build in SearchScreen.kt

--- app/src/main/java/hr/foi/air/servicesync/ui/screens/SearchScreen.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/screens/SearchScreen.kt
@@ -1,9 +1,12 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package hr.foi.air.servicesync.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

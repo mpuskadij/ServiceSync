450357bd5bbfa1cca366725873513bedd88aba76
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-23 14:19:19 +0100

    FTR-81: -added IMapProviderStateManager interface on business logic layer for future use - added MapProviderManager with properties and methods: KEY - under which key the name of the map provider will be saved mapProviders - loads all implementations of the interface IMapProvider getAllProviders - returns the mapProviders saveMapProvider - saves the chosen map provider using its name getCurrentMapProvider - tries to get the saved map provider. If it can't, returns the first implementation found in service loader

---
+++ app/src/main/java/hr/foi/air/servicesync/business/MapProviderManager.kt
@@ -0,0 +1,26 @@
+package hr.foi.air.servicesync.business
+
+import android.content.Context
+import com.example.maps.interfaces.IMapProvider
+import hr.foi.air.servicesync.backend.SharedPreferencesManager
+import java.util.ServiceLoader
+
+object MapProviderManager : IMapProviderStateManager {
+    private const val KEY = "MAP_PROVIDER"
+    private val mapProviders by lazy {
+        ServiceLoader.load(IMapProvider::class.java).toList()
+    }
+
+    override fun getAllProviders() : List<IMapProvider> {
+        return mapProviders
+    }
+
+    override fun saveMapProvider(context: Context, mapProviderName: String) {
+        SharedPreferencesManager.saveStringPreference(context,KEY,mapProviderName)
+    }
+
+    override fun getCurrentMapProvider(context: Context) : String {
+        return SharedPreferencesManager.getMapProvider(context, KEY) ?: mapProviders.first().getName()
+    }
+
+}

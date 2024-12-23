393c68342a8b5a39b19b5d7181e03312f17cfcd0
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-23 15:42:04 +0100

    FTR-81: -updated CompanyLocation which uses MapProviderManager to get the currently selected map provider and calls the function CreateMap which creates the map -updated MapDropdown so that it requires the initial map provider name to fill the text field, also added a lambda function to call when a new map provider is selected -updated ProfileContent so that it gets the current name of the map provider and passes it into MapDropdown, after MapDropdown call the lambda function, ProfileContent updates the name of the current map provider -after all changes to the user's profile are made, ProfileContent calls MapProviderManager to save the new map provider -added a new ProfileInfoBox component to display the current map provider

--- app/src/main/java/hr/foi/air/servicesync/ui/components/CompanyLocation.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/components/CompanyLocation.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.GeoPoint
+import hr.foi.air.servicesync.business.MapProviderManager
 
 @Composable
 fun CompanyLocation(geoPoint: GeoPoint)
 {
     Box(modifier = Modifier.fillMaxWidth().height(200.dp)) {
+        MapProviderManager.getCurrentMapProvider(context = LocalContext.current).CreateMap(geoPoint.latitude,geoPoint.longitude)
     }
 }
--- app/src/main/java/hr/foi/air/servicesync/ui/components/MapDropdown.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/components/MapDropdown.kt
@@ -1,11 +1,17 @@
 package hr.foi.air.servicesync.ui.components
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,24 +21,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.maps.interfaces.IMapProvider
 import hr.foi.air.servicesync.R
+import hr.foi.air.servicesync.business.MapProviderManager
 import java.util.ServiceLoader
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MapDropdown() {
-    val mapProviders by lazy {
-        ServiceLoader.load(IMapProvider::class.java).toList()
-    }
+fun MapDropdown(mapProviderName: String,onMapProviderChange : (String) -> Unit) {
+    val  mapProviders = MapProviderManager.getAllProviders()
     var expanded by remember { mutableStateOf(false) }
 
     ExposedDropdownMenuBox(
         expanded = expanded,
-        onExpandedChange = {expanded = it},
-        modifier = Modifier.fillMaxWidth()
+        onExpandedChange = {expanded = !expanded},
+        modifier = Modifier
     ){
-        val noProviders = stringResource(R.string.no_providers_found)
-        var chosenMapProvider by remember { mutableStateOf(mapProviders.firstOrNull()?.getName() ?: noProviders
-        ) }
+        var chosenMapProvider by remember { mutableStateOf(mapProviderName)
+         }
         TextField(
             label = {
                 Text(text = stringResource(R.string.choose_map_type)) },
@@ -40,8 +44,12 @@ fun MapDropdown() {
             onValueChange = {
                 chosenMapProvider = it
             },
-            enabled = false
-
+            readOnly = true,
+            enabled = false,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            modifier = Modifier.fillMaxWidth().menuAnchor(MenuAnchorType.PrimaryNotEditable, expanded).clickable { expanded  =true }
         )
         ExposedDropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}) {
             mapProviders.forEach {
@@ -52,6 +60,7 @@ fun MapDropdown() {
                     onClick = {
                         expanded = false
                         chosenMapProvider = it.getName()
+                        onMapProviderChange(chosenMapProvider)
                     }
                 )
             }

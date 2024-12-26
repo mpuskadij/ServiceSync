0335d908e509149b291be5fbbc6b45a35e095a32
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-26 12:19:27 +0100

    FTR-81: -removed TODO for MapProviderManager.getCurrentMapProvider -added private fun MapProviderManager.getFirstMapProviderOrErrorMessage to return the name of the first loaded map provider or a error message if currently no map providers exist -changed MapProviderManager.getCurrentMapProviderName to return a error message is currently no providers exist

--- app/src/main/java/hr/foi/air/servicesync/business/MapProviderManager.kt
+++ app/src/main/java/hr/foi/air/servicesync/business/MapProviderManager.kt
@@ -2,6 +2,7 @@ package hr.foi.air.servicesync.business
 
 import android.content.Context
 import com.example.maps.interfaces.IMapProvider
+import hr.foi.air.servicesync.R
 import hr.foi.air.servicesync.backend.SharedPreferencesManager
 import java.util.ServiceLoader
 
@@ -9,6 +10,10 @@ object MapProviderManager : IMapProviderStateManager {
     private const val KEY = "MAP_PROVIDER"
     private val mapProviders by lazy {
         ServiceLoader.load(IMapProvider::class.java).toList()
+    }
+
+    private fun getFirstMapProviderOrErrorMessage(context: Context) : String {
+        return mapProviders.firstOrNull()?.getName() ?: context.getString(R.string.no_available_maps)
     }
 
     override fun getAllProviders() : List<IMapProvider> {
@@ -19,18 +24,16 @@ object MapProviderManager : IMapProviderStateManager {
         SharedPreferencesManager.saveStringPreference(context,KEY,mapProviderName)
     }
 
-    //TODO this will crash the app if there are no map providers. To remove that possibility, consider checking if there are any providers and if not, dont try to draw the map
     override fun getCurrentMapProviderName(context: Context) : String {
-        val storedMapProvider = SharedPreferencesManager.getMapProvider(context, KEY) ?: mapProviders.first().getName()
+        val storedMapProvider = SharedPreferencesManager.getMapProvider(context, KEY) ?: getFirstMapProviderOrErrorMessage(context)
         val foundProvider = mapProviders.any { provider -> provider.getName() == storedMapProvider }
-        if (foundProvider) {
-            return storedMapProvider
-        }
-        else {
-            return mapProviders.first().getName()
+        return if (foundProvider) {
+            storedMapProvider
+        } else {
+            getFirstMapProviderOrErrorMessage(context)
         }
     }
-    //TODO should return null if no map provider exists so that a map will not be drawn
+
     override fun getCurrentMapProvider(context: Context): IMapProvider? {
         return mapProviders.find {mapProvider -> mapProvider.getName() == getCurrentMapProviderName(context) }
     }

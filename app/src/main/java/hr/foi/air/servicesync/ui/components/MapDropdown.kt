4ea685072596bc7c7abc7c395a3dd6b1d0a3c3d0
Marin Puškadija <marin.puskadija@gmail.com>
2024-12-26 13:16:13 +0100

    FTR-81: -fixed dropdown to not be visually disabled because it will give the impression to the user that it is not clickable

--- app/src/main/java/hr/foi/air/servicesync/ui/components/MapDropdown.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/components/MapDropdown.kt
@@ -1,16 +1,15 @@
 package hr.foi.air.servicesync.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,10 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.example.maps.interfaces.IMapProvider
 import hr.foi.air.servicesync.R
 import hr.foi.air.servicesync.business.MapProviderManager
-import java.util.ServiceLoader
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,6 +44,10 @@ fun MapDropdown(mapProviderName: String,onMapProviderChange : (String) -> Unit) 
             },
             readOnly = true,
             enabled = false,
+            colors = TextFieldDefaults.colors(
+                disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                disabledTextColor = MaterialTheme.colorScheme.onSurface
+            ),
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
             },

832f858d3ffbffe3befce3b7b69b5f8ec89f2552
Megica <mmarkovin21@foi.hr>
2025-01-07 15:29:36 +0100

    FTR-89: Added new function for calculating rating average

--- app/src/main/java/hr/foi/air/servicesync/business/ReviewHandler.kt
+++ app/src/main/java/hr/foi/air/servicesync/business/ReviewHandler.kt
@@ -1,13 +1,32 @@
 package hr.foi.air.servicesync.business
 
+import android.annotation.SuppressLint
 import hr.foi.air.servicesync.backend.FirestoreReviews
 import hr.foi.air.servicesync.data.Review
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class ReviewHandler(
     private val firestoreReviews: FirestoreReviews = FirestoreReviews()
 ) {
+    private var reviewList: List<Review> = emptyList()
+
     fun fetchReviews(companyName: String, onResult: (List<Review>) -> Unit) {
-        firestoreReviews.fetchReviewsForCompany(companyName, onResult)
+        firestoreReviews.fetchReviewsForCompany(companyName) { reviews ->
+            reviewList = reviews
+            onResult(reviews)
+        }
+    }
+
+    fun fetchAndCalculateReviewAverage(
+        companyName: String,
+        onResult: (Double) -> Unit
+    ) {
+        firestoreReviews.fetchReviewsForCompany(companyName) { reviews ->
+            val average = if (reviews.isEmpty()) 0.0 else reviews.map { it.rating }.average()
+            val roundedAverage = BigDecimal(average).setScale(1, RoundingMode.HALF_UP).toDouble()
+            onResult(roundedAverage)
+        }
     }
 
     fun addReview(review: Review, onResult: (Boolean) -> Unit) {

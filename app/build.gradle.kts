f2fdc5674acf1a88d4297018ea77f8c522b8ed92
Ivan B. <ivan.beusan@yahoo.com>
2025-01-15 21:32:06 +0100

    FTR-100 Added necessary dependencies in gradle for profile photo changing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,12 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("io.coil-kt:coil-compose:2.2.0")
+
     // QR implementation
     implementation("androidx.camera:camera-core:1.4.0")
     implementation("androidx.camera:camera-camera2:1.4.0")

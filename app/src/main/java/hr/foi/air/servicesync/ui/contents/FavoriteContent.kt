cb63210eee5248eeead52c99ba4c19a822e6b735
Ivan B. <ivan.beusan@yahoo.com>
2025-01-16 23:10:54 +0100

    FTR-14 FTR-112 FTR-113 FTR-114 FTR-115 Implemented UI that handles fetching favorite companies for logged user

--- app/src/main/java/hr/foi/air/servicesync/ui/contents/FavoriteContent.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/contents/FavoriteContent.kt
@@ -1,43 +1,26 @@
 package hr.foi.air.servicesync.ui.contents
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.google.firebase.firestore.FieldPath
-import com.google.firebase.firestore.FirebaseFirestore
+import hr.foi.air.servicesync.business.FavoritesHandler
 import hr.foi.air.servicesync.business.ReviewHandler
 import hr.foi.air.servicesync.data.UserSession
 import hr.foi.air.servicesync.ui.components.CompanyCard
-
-
-val userId = UserSession.username
-
-
-fun getFavoriteCompanies(userId: String, onResult: (List<String>) -> Unit) {
-    val db = FirebaseFirestore.getInstance()
-    db.collection("userFavorites")
-        .whereEqualTo("userId", userId)
-        .get()
-        .addOnSuccessListener { result ->
-            val companyIds = result.documents.mapNotNull { it.getString("companyId") }
-            onResult(companyIds)
-        }
-        .addOnFailureListener { exception ->
-            Log.e("Favorites", "Error fetching favorites: ${exception.message}")
-            onResult(emptyList())
-        }
-}
 
 @Composable
 fun FavoriteContent(
@@ -46,34 +29,18 @@ fun FavoriteContent(
 ) {
     val companies = remember { mutableStateOf(emptyList<Map<String, Any>>()) }
     val isLoading = remember { mutableStateOf(true) }
+    val favoriteHandler = remember { FavoritesHandler() }
 
     LaunchedEffect(Unit) {
         isLoading.value = true
 
-        // Dohvati popis omiljenih companyId (ne cijele tvrtke)
-        getFavoriteCompanies(UserSession.username) { favoriteCompanyIds ->
-            if (favoriteCompanyIds.isNotEmpty()) {
-                val db = FirebaseFirestore.getInstance()
-                db.collection("companies")
-                    .whereIn(FieldPath.documentId(), favoriteCompanyIds) // Filtriraj po ID-jevima
-                    .get()
-                    .addOnSuccessListener { documents ->
-                        companies.value = documents.map { it.data }
-                        isLoading.value = false
-                    }
-                    .addOnFailureListener {
-                        companies.value = emptyList()
-                        isLoading.value = false
-                    }
-            } else {
-                companies.value = emptyList()
-                isLoading.value = false
-            }
+        favoriteHandler.getFavoriteCompanies(UserSession.username) { favoriteCompanies ->
+            companies.value = favoriteCompanies
+            isLoading.value = false
         }
     }
 
     if (isLoading.value) {
-        // Prikaži loader dok se učitavaju podaci
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -82,33 +49,47 @@ fun FavoriteContent(
             CircularProgressIndicator()
         }
     } else {
-        // Prikaži omiljene firme
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(companies.value) { company ->
-                val name = company["name"] as? String ?: "No Name"
-                val category = company["category"] as? String ?: "No Category"
-                val imageUrl = company["pictureURL"] as? String
+        Column (
+            modifier = Modifier
+                .padding(16.dp, 50.dp, 16.dp, 0.dp)
+        ){
+            if (companies.value.isEmpty()) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "No favorites", style = MaterialTheme.typography.headlineLarge)
+                }
+            } else {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(companies.value) { company ->
+                        val name = company["name"] as? String ?: "No Name"
+                        val category = company["category"] as? String ?: "No Category"
+                        val imageUrl = company["pictureURL"] as? String
 
-                val reviewsHandler = ReviewHandler()
-                val reviewAverages = remember { mutableStateOf(mapOf<String, Double>()) }
+                        val reviewsHandler = ReviewHandler()
+                        val reviewAverages = remember { mutableStateOf(mapOf<String, Double>()) }
 
-                val averageRating = reviewAverages.value[name] ?: "Loading..."
+                        val averageRating = reviewAverages.value[name] ?: "Loading..."
 
-                if (!reviewAverages.value.containsKey(name)) {
-                    reviewsHandler.fetchAndCalculateReviewAverage(name) { average ->
-                        reviewAverages.value += (name to average)
+                        if (!reviewAverages.value.containsKey(name)) {
+                            reviewsHandler.fetchAndCalculateReviewAverage(name) { average ->
+                                reviewAverages.value += (name to average)
+                            }
+                        }
+
+                        CompanyCard(
+                            companyName = name,
+                            companyCategory = category,
+                            companyRating = averageRating.toString(),
+                            imageUrl = imageUrl,
+                            onCardClick = {
+                                navController.navigate("company/$name")
+                            }
+                        )
                     }
                 }
-
-                CompanyCard(
-                    companyName = name,
-                    companyCategory = category,
-                    companyRating = averageRating.toString(),
-                    imageUrl = imageUrl,
-                    onCardClick = {
-                        navController.navigate("company/$name")
-                    }
-                )
             }
         }
     }

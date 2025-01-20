556c6f3a3d7a89a3529c761b7a90ed9e5303087b
Megica <mmarkovin21@foi.hr>
2025-01-20 08:24:48 +0100

    FTR-91: Corrected colors using isDark method

--- app/src/main/java/hr/foi/air/servicesync/ui/components/ReservationItemDone.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/components/ReservationItemDone.kt
@@ -20,6 +20,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.compose.onPrimaryContainerDark
+import com.example.compose.onPrimaryContainerLight
+import com.example.compose.onPrimaryDark
+import com.example.compose.onPrimaryLight
+import com.example.compose.onSecondaryDark
+import com.example.compose.onSecondaryLight
+import com.example.compose.onSurfaceDark
+import com.example.compose.onSurfaceLight
+import com.example.compose.onSurfaceVariantDark
+import com.example.compose.onSurfaceVariantLight
+import com.example.compose.surfaceContainerDark
+import com.example.compose.surfaceContainerLight
+import com.example.compose.surfaceContainerLowDark
+import com.example.compose.surfaceContainerLowLight
 import hr.foi.air.servicesync.R
 import hr.foi.air.servicesync.business.formatDate
 import hr.foi.air.servicesync.data.UserSession
@@ -39,7 +53,7 @@ fun ReservationItemDone(
             .padding(8.dp),
         shape = MaterialTheme.shapes.medium,
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceContainer
+            containerColor = isDark(surfaceContainerDark, surfaceContainerLight)
         ),
     ) {
         Row(
@@ -57,13 +71,13 @@ fun ReservationItemDone(
                     style = MaterialTheme.typography.bodyLarge.copy(
                         fontWeight = FontWeight.Bold
                     ),
-                    color = MaterialTheme.colorScheme.secondary
+                    color = isDark(onSurfaceVariantDark, onSurfaceVariantLight)
                 )
                 Spacer(modifier = Modifier.height(2.dp))
                 Text(
                     text = formatDate(reservationDate),
                     style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.secondary
+                    color = isDark(onSurfaceDark, onSurfaceLight)
                 )
             }
             Row (
@@ -75,14 +89,15 @@ fun ReservationItemDone(
                     },
                     colors = ButtonDefaults.filledTonalButtonColors(
                         containerColor = MaterialTheme.colorScheme.secondary,
-                        contentColor = MaterialTheme.colorScheme.onSecondary
+                        contentColor = isDark(onPrimaryContainerDark, onPrimaryContainerLight)
                     ),
                     enabled = buttonEnabled
                 ) {
-                    Text(text = if (buttonEnabled)
+                    Text(
+                        text = if (buttonEnabled)
                         stringResource(R.string.leave_review) else stringResource(
-                        R.string.review_submited
-                    )
+                        R.string.review_submited),
+                        color = isDark(onPrimaryDark, onPrimaryLight)
                     )
                 }
             }

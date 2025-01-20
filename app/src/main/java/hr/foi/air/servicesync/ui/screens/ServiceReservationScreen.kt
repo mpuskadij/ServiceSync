0f9f8ced21a8d110c2a1bc2a88b66c21720b7d34
Ivan B. <ivan.beusan@yahoo.com>
2025-01-20 10:47:38 +0100

    FTR-14 Removed unused imports

--- app/src/main/java/hr/foi/air/servicesync/ui/screens/ServiceReservationScreen.kt
+++ app/src/main/java/hr/foi/air/servicesync/ui/screens/ServiceReservationScreen.kt
@@ -2,25 +2,34 @@ package hr.foi.air.servicesync.ui.screens
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.compose.inversePrimaryDarkMediumContrast
-import com.example.compose.inversePrimaryLightMediumContrast
 import com.example.compose.onSurfaceDark
 import com.example.compose.onSurfaceLight
-import com.example.compose.onTertiaryDark
-import com.example.compose.surfaceDark
 import com.example.compose.surfaceDimLightHighContrast
-import com.example.compose.surfaceLight
 import com.example.compose.surfaceVariantDark
 import com.example.compose.surfaceVariantLight
 import com.example.compose.tertiaryDark
@@ -34,7 +43,7 @@ import hr.foi.air.servicesync.ui.components.BackButton
 import hr.foi.air.servicesync.ui.components.ReservationCalendar
 import hr.foi.air.servicesync.ui.components.isDark
 import java.text.DateFormat
-import java.util.*
+import java.util.Date
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
